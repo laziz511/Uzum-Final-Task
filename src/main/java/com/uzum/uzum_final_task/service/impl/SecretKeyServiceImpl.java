8439a8cc64d9e7c8b984506004095e6eefd8dab7
Laziz <lazizo2004@gmail.com>
2023-12-01 01:29:48 +0500

    add SecretKeyServiceImpl

---
+++ src/main/java/com/uzum/uzum_final_task/service/impl/SecretKeyServiceImpl.java
@@ -0,0 +1,46 @@
+package com.uzum.uzum_final_task.service.impl;
+
+import com.uzum.uzum_final_task.entity.SecretKey;
+import com.uzum.uzum_final_task.repository.SecretKeyRepository;
+import com.uzum.uzum_final_task.service.SecretKeyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SecretKeyServiceImpl implements SecretKeyService {
+    private final SecretKeyRepository secretKeyRepository;
+
+    @Value("${secret.key.file.path}")
+    private String secretKeyFilePath;
+
+    public void initializeSecretKey() {
+        try {
+            String secretKeyValue = readSecretKeyFromFile();
+            saveSecretKeyToDatabase(secretKeyValue);
+        } catch (IOException e) {
+            log.warn("Failed to read the secret key file.", e);
+        }
+    }
+
+    private String readSecretKeyFromFile() throws IOException {
+        ClassPathResource resource = new ClassPathResource(secretKeyFilePath);
+        return new String(resource.getInputStream().readAllBytes());
+    }
+
+    private void saveSecretKeyToDatabase(String secretKeyValue) {
+        if (secretKeyRepository.findByKeyValue(secretKeyValue).isEmpty()) {
+            SecretKey secretKey = new SecretKey();
+            secretKey.setKeyValue(secretKeyValue);
+            secretKeyRepository.save(secretKey);
+        }
+    }
+}
