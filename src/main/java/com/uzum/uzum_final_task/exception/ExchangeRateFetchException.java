c05d1b735655c2f2b205a1e59d1ef7ef17aba3c2
Laziz <lazizo2004@gmail.com>
2023-12-01 00:34:19 +0500

    remove ExchangeRateFetchException.java

--- src/main/java/com/uzum/uzum_final_task/exception/ExchangeRateFetchException.java
+++
@@ -1,8 +0,0 @@
-package com.uzum.uzum_final_task.exception;
-
-public class ExchangeRateFetchException extends RuntimeException {
-    public ExchangeRateFetchException(String message) {
-        super(message);
-    }
-
-}
