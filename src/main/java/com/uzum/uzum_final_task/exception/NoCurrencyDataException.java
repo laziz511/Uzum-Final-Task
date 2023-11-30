9fd0cff329e9b62eed79220223cad1821c3bf823
Laziz <lazizo2004@gmail.com>
2023-12-01 00:34:39 +0500

    remove NoCurrencyDataException.java

--- src/main/java/com/uzum/uzum_final_task/exception/NoCurrencyDataException.java
+++
@@ -1,7 +0,0 @@
-package com.uzum.uzum_final_task.exception;
-
-public class NoCurrencyDataException extends RuntimeException {
-    public NoCurrencyDataException(String message) {
-        super(message);
-    }
-}
